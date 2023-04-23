369050ba138017b819f6cec5e825e6b6ce735354
murakamiiii <jiw46.xe@gmail.com>
2023-04-23 09:00:38 +0900

    fix error

--- src/background.ts
+++ src/background.ts
@@ -7,9 +7,16 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
+    // get tabId as number, if undefined, alert and early return
+    const tabId = tab?.id;
+    if (typeof tabId !== "number") {
+        alert("error in link to calil. the tabID is undefined");
+        return;
+    }
+    
     if (info.menuItemId === "link to Calil") {
         chrome.scripting.executeScript({
-            target: { tabId: tab.id },
+            target: { tabId: tabId },
             files: ["content.js"],
         });
     }
