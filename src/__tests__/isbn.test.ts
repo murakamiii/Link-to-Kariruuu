f0de799718d2ccca59ae9ab50dc1898351641d0b
murakamiiii <jiw46.xe@gmail.com>
2023-05-03 20:15:11 +0900

    fix error

--- src/__tests__/isbn.test.ts
+++ src/__tests__/isbn.test.ts
@@ -37,7 +37,7 @@ describe('getISBNFromSelection', () => {
 
     test('should return ISBN when valid ISBN is in the selection', () => {
         const result = getISBNFromSelection();
-        expect(result).toBe('9781234567890xxxxxxxxxxxxxx');
+        expect(result).toBe('9781234567890');
     });
 
     test('should return null when no ISBN is in the selection', () => {
